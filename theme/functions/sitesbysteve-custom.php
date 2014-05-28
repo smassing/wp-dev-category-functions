80d9adc40a747399a08def70cb284dc7aa5730f1
smassing <steve.massing@gmail.com>
2014-05-28 16:13:04 -0600

    initial commit of code:

---
+++ theme/functions/sitesbysteve-custom.php
@@ -0,0 +1,60 @@
+<?php
+
+
+function getCats($included, $excluded)
+{
+
+	//gett he list of included categories
+	$included_out = "";
+
+	foreach ($included as $itm) 
+	{
+
+		$temp = get_cat_id($itm);
+		if (strlen($included_out)>0) 
+		{
+			$included_out = $included_out . "," . $temp;
+		} else {
+			$included_out = $temp;
+		}
+
+	}
+
+	//get the list of exclusions
+	$excluded_out = "";
+
+	foreach ($excluded as $itm) 
+	{
+
+		$temp = get_cat_id($itm);
+		if (strlen($excluded_out)>0) 
+		{
+			$excluded_out = $excluded_out . ",-" . $temp;
+		} else {
+			$excluded_out = "-" . $temp;
+		}
+
+	}
+
+	//return the final list of categories  eg. 1,2,3,-4,-5,-6
+	if (strlen($included_out)>0) 
+	{
+		if (strlen($excluded_out)>0) 
+		{
+			return $included_out . "," .$excluded_out;
+		} else {
+			return $included_out;
+		}
+	} else {
+		return $excluded_out;
+	}
+
+
+}
+
+
+
+
+
+
+?>
